513f153890ebbb6227ec4012cc85e7072fec6e66
ytokarevskaya <lan2214@yandex.ru>
2020-09-11 10:28:58 +0300

    Fix node.js problem + add icon fonts

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const app = express();
- 
-app.use(function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+const http = require("http");
+const fs = require("fs");
+  
+http.createServer(function(request, response){
+    let filePath = request.url.substr(1);
+    if (request.url === "/") filePath = "index.html";
+
+    fs.access(filePath, fs.constants.R_OK, err => {
+        if(err){
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }
+        else{
+            fs.createReadStream(filePath).pipe(response);
+        }
+      });
+}).listen(3000, function(){
+    console.log("Server started at 3000");
 });
- 
-app.listen(3000);
